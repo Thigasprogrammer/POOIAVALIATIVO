6de7a0edc0ec0a3668bc491ac566f731c8eafac6
Thigasprogrammer <thiagoaguiar035@gmail.com>
2025-06-11 19:50:59 -0300

    Add files via upload

---
+++ Sistema.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Sistema{
+	public static void main(String[] args){
+		HashMap<String, UF>  		
+
+
+
+	}//main
+}//class
---
+++ UF.java
@@ -0,0 +1,19 @@
+public class UF{
+	String nome;
+	String sigla;
+	String regiao;
+
+	public UF(String nome, String sigla, String regiao){
+		this.nome = nome;
+		this.sigla = sigla;
+		this.regiao = regiao;
+	}
+
+	public String toString(){
+		return "Nome: " + nome + "\n" +
+				"Sigla: " + sigla + "\n" +
+				"Região: " + regiao + "\n";
+	}
+
+
+}//class
