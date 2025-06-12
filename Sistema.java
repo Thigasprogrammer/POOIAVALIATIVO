aa4030a1b1567bb0c47aaa4a6ceb402cf11721a4
Thigasprogrammer <thiagoaguiar035@gmail.com>
2025-06-11 21:55:00 -0300

    Add files via upload

--- Sistema.java
+++ Sistema.java
@@ -3,9 +3,49 @@ import java.util.Scanner;
 
 public class Sistema{
 	public static void main(String[] args){
-		HashMap<String, UF>  		
+		//definindo variaveis
+		Scanner teclado = new Scanner(System.in);
+		HashMap<String, UF> hmcadastros = new HashMap<>();
+		String nome;
+		String sigla;
+		String regiao;
+		String pesquisa;
+
+		System.out.print("Digite o nome da UF, caso deseje sair digite 'sair': ");
+		nome = teclado.nextLine();
+
+		while(!(nome.equalsIgnoreCase("sair"))){
+			System.out.print("Digite a sigla da UF: ");
+			sigla = teclado.nextLine();
+			System.out.print("Digite a região da UF: ");
+			regiao = teclado.nextLine();
+			UF cadastramento = new UF(nome, sigla, regiao);
+			hmcadastros.put(sigla, cadastramento);
+			System.out.print("Digite o nome da UF, caso deseje sair digite 'sair': ");
+			nome = teclado.nextLine();
 
 
+		}// while
+
+		System.out.print("Digite a sigla da UF ou da regiao, que deseja pesquisar, caso deseje sair digite 'sair': ");
+		pesquisa = teclado.nextLine();
+
+		while(!(pesquisa.equalsIgnoreCase("sair"))){ // consulta
+			if(hmcadastros.containsKey(pesquisa)){ // consulta por uf
+				System.out.print(hmcadastros.get(pesquisa).toString());
+			}
+			else{ //consulta por regiao
+				for(UF uf : hmcadastros.values()){
+					if(uf.regiao.equalsIgnoreCase(pesquisa)) {
+                        System.out.println(uf.toString());
+				}
+			}
+			System.out.print("Digite a sigla da UF ou da regiao, que deseja pesquisar, caso deseje sair digite 'sair': ");
+			pesquisa = teclado.nextLine();
+
+		}//while2
+		System.out.print("----------fim do programa----------");
+		teclado.close();
 
 	}//main
 }//class
